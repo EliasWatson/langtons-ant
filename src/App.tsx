bc759a0eb72cfb90cddc0e22c39c18c6318590ed
Elias Watson <me@eliaswatson.dev>
2025-08-02 11:28:02 -0400

    Add pixi app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { Application } from "@pixi/react";
+
 export default function App() {
-  return null;
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <Application
+        className="absolute left-0 top-0"
+        resizeTo={window}
+        autoDensity={true}
+        resolution={devicePixelRatio}
+      ></Application>
+    </div>
+  );
 }
